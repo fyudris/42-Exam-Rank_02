c414c7fd3e23636aa32160c8a7d5249028f6a0f7
fyudris <fanny.yudris@gmail.com>
2025-05-21 17:05:42 +0200

    Adding solution to ft_strcpy

--- Level_01/ft_strcpy/ft_strcpy.c
+++ Level_01/ft_strcpy/ft_strcpy.c
@@ -1,34 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fyudris <fyudris@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/21 16:39:02 by fyudris           #+#    #+#             */
-/*   Updated: 2025/05/21 17:05:13 by fyudris          ###   ########.fr       */
+/*   Created: 2025/05/21 16:57:12 by fyudris           #+#    #+#             */
+/*   Updated: 2025/05/21 17:04:58 by fyudris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putchar(char c)
+char	*ft_strcpy(char *dest, const char *src)
 {
-	write(1, &c, 1);
-}
+	unsigned int	i;
 
-void    ft_putstr(char *str)
-{
-	while (*str)
+	i = 0;
+	while (src[i])
 	{
-		ft_putchar(*str);
-		str++;
+		dest[i] = src[i];
+		i++;
 	}
+	dest[i] = '\0';
+	return (dest);
 }
 /*
-int	main()
+#include <stdio.h>
+
+int	main ()
 {
-	ft_putstr("");
+	char dest[10];
+	char *src = "HELLO";
+
+	ft_strcpy(dest, src);
+	printf("%s",dest);
 	return (0);
 }
 */
